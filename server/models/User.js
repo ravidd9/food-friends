aef025888011e42c52b68c996adce64fb48c9138
Ravid Cohen <ravidd9@gmail.com>
2019-05-28 15:57:26 +0300

    s

--- server/models/User.js
+++ server/models/User.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+    
     firstName: String,
     lastName: String,
     birthDay: Date,
     email : String,
     password : String, 
     interests : [String], 
-    profilePic : String,
+    profilePic : String
 })
 
 const User = mongoose.model('User', userSchema, "Users")
