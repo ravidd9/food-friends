2a8663ac6848dc4308e4103bb383abcb34b50d64
yossiD <dagangm@gmail.com>
2019-05-28 10:50:39 +0300

    schema + basic homepage setup

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import '../style/HomePage.css';
+import FoodContainer from './FoodContainer';
+import Filters from './Filters';
 
 
 class HomePage extends Component {
     render() {
         return (
             <div id="homePage">
-                
+                <FoodContainer/>
+                <Filters/>
             </div>
         );
     }
 }
-
+    
 export default HomePage;
