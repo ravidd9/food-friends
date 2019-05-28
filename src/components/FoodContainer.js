e240bfb03a276ea492f637b6b6022fc9a1fb3c0b
Ravid Cohen <ravidd9@gmail.com>
2019-05-28 11:23:53 +0300

    starting the components

--- src/components/FoodContainer.js
+++ src/components/FoodContainer.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import '../style/FoodContainer.css';
+import { observer, inject } from 'mobx-react';
 
 
+@inject("generalStore")
+@observer
 class FoodContainer extends Component {
     render() {
         return (
             <div id="foodContainer">
-                Hi
+                food container
             </div>
         );
     }
