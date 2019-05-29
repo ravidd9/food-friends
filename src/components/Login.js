eb5dff4f5eee1b4f23cf8482ad2bc0344b6fde2e
Ravid Cohen <ravidd9@gmail.com>
2019-05-29 09:41:18 +0300

    starting login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,22 @@ import '../style/Login.css';
 
 
 class Login extends Component {
+
+
     render() {
         return (
             <div id="login">
-                
+                <div>Already a member?</div>
+                <div id="loginForm">
+                    <div>Email</div>
+                    <input type="text" placeholder="Enter Email" name="email" />
+                    <div>Password</div>
+                    <input type="text" placeholder="Enter Password" name="password" />
+                </div>
+                <div>Login</div>
+                <div>Not a member?, 
+                    
+                </div>
             </div>
         );
     }
