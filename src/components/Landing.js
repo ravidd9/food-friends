79858c92e25d4090545969d3976125754ca82dfd
Ravid Cohen <ravidd9@gmail.com>
2019-05-29 09:34:07 +0300

    updated local db

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import '../style/Landing.css';
+import Login from './Login';
 
 
 class Landing extends Component {
     render() {
         return (
             <div id="landing">
-                
+                <h2>Welcome to Food-Friends</h2>
+                <h4>the best way to find your dining mate!</h4>
+                <Login />
             </div>
         );
     }
