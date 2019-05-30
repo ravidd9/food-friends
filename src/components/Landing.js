8f306461b99e7451c900fad43225aa28021fe6e0
yossiD <dagangm@gmail.com>
2019-05-30 10:32:49 +0300

    redirect to home if logged in

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../style/Landing.css';
 import Login from './Login';
 import Register from './Register';
+import { Redirect } from 'react-router-dom'
+
 
 
 class Landing extends Component {
@@ -12,18 +14,27 @@ class Landing extends Component {
         }
     }
 
-    changeLogin = () =>{
-        this.setState({login: !this.state.login})
+    changeLogin = () => {
+        this.setState({ login: !this.state.login })
     }
 
     render() {
+
+        let isLoggedIn = JSON.parse(sessionStorage.getItem('login'))
+
         return (
-            <div id="landing">
-                <h2>Welcome to Food-Friends</h2>
-                <h4>the best way to find your dining mate!</h4>
-                {this.state.login ?
-                    <Login changeLogin={this.changeLogin}/> :
-                    <Register changeLogin={this.changeLogin} />}
+            <div>
+
+                {isLoggedIn ? <Redirect to="/home" />
+                    :
+                    <div id="landing">
+                        <h2>Welcome to Food-Friends</h2>
+                        <h4>the best way to find your dining mate!</h4>
+                        {this.state.login ?
+                            <Login changeLogin={this.changeLogin} /> :
+                            <Register changeLogin={this.changeLogin} />}
+                    </div>}
+
             </div>
         );
     }
