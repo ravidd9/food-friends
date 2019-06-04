d19eb319926ca72c5f78fa10710a82b682c8ba29
Ravid Cohen <ravidd9@gmail.com>
2019-06-04 15:47:42 +0300

    fixes

--- src/stores/GeneralStore.js
+++ src/stores/GeneralStore.js
@@ -238,6 +238,7 @@ export class GeneralStore {
     }
 
     @action matchUsers = async email => {
+        console.log(this.currentUser)
         this.currentUser.matchedWith.push(email)
         await this.updateUser('matchedWith')
 
