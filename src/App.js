0caaa5df3d5aa31a89573090041eef965978091b
yossiD <dagangm@gmail.com>
2019-06-05 13:04:44 +0300

    page transitions basic setup

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TemporaryDrawer from './components/TemporaryDrawer';
 import Profile from './components/Profile';
 import SimpleSnackbar from './components/SimpleSnackbar';
 
+const swup = new swup()
 
 
 
